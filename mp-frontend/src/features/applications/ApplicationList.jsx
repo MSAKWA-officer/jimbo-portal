5dd00579e2dda4a80b3fa4226418d4832de79952
MSAKWA-officer <allanmsakwa@gmail.com>
2026-09-14 19:26:50 +0300

    fix sidebar

--- mp-frontend/src/features/applications/ApplicationList.jsx
+++ mp-frontend/src/features/applications/ApplicationList.jsx
@@ -144,10 +144,6 @@ export default function ApplicationList() {
           <h1 className="text-3xl font-bold text-black">
             Applications
           </h1>
-
-          <p className="text-base text-black mt-1">
-            Full list of all submitted applications.
-          </p>
         </div>
 
         <Link
